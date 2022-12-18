c75b173bddde4926b63c34202f5f6f588e32de30
Logan <79545798+Logan-proj@users.noreply.github.com>
2022-12-18 09:18:21 -0500

    Celsius to Fahrenheit Converter

---
+++ Checkpoint1.swift
@@ -0,0 +1,7 @@
+import UIKit
+
+// Celsius to Fahrenheit Converter
+let tempCelsius = 0.0
+let tempFahrenheit = tempCelsius * 9 / 5 + 32
+let message = "\(tempCelsius)° celsius converts to \(tempFahrenheit)° fahrenheit."
+print(message)
